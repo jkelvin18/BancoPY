c65a86556c26bdb38c8f3cf19d97e8e400d6df59
jkelvin18 <jkelvin18@hotmail.com>
2021-06-16 21:41:54 -0300

    teste das classes conta e cliente

--- teste.py
+++ teste.py
@@ -3,5 +3,13 @@ from models.conta import Conta
 
 felicity: Cliente = Cliente('Felicity', 'felicity@email', '123.456.789-81', '02/09/1972')
 
+jao: Cliente = Cliente('jao', 'felicity@email', '123.456.739-81', '02/09/1972')
+
 print(felicity)
+
+Contaf : Conta = Conta(felicity)
+contae : Conta = Conta(jao)
+
+print (Contaf)
+print (contae)
 
